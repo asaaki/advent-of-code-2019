27ea19b2ddfdd4e8a1435420f558d2fe6eddfc81
Christoph Grabo <asaaki@mannaz.cc>
2019-12-05 02:30:54 +0100

    Simplify and clean up day 4

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -7,13 +7,7 @@ use std::io::Read;
 
 type Code = isize;
 type CodeRange = std::ops::RangeInclusive<Code>;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Passphrase {
-    code: Code,
-}
-
-type Passphrases = Vec<Passphrase>;
+type Passwords = Vec<Code>;
 
 fn main() -> std::io::Result<()> {
     let clap_app = App::new("AoC day 4")
@@ -31,11 +25,12 @@ fn main() -> std::io::Result<()> {
     let input_file_path = fs::canonicalize(matches.value_of("INPUT").unwrap())?;
     let file = File::open(&input_file_path)?;
     let pass_range = read_data(file);
-    let pass_phrases = calculate_passphrases(pass_range);
-    let strict_pass_phrases = calculate_passphrases_strict(pass_phrases.clone());
+    let passwords = calculate_passwords(pass_range);
+    let normal_length = passwords.len();
+    let strict_passwords = calculate_passwords_strict(passwords);
 
-    println!("Part 1: {}", pass_phrases.len());
-    println!("Part 2: {}", strict_pass_phrases.len());
+    println!("Part 1: {}", normal_length);
+    println!("Part 2: {}", strict_passwords.len());
 
     Ok(())
 }
@@ -51,46 +46,61 @@ fn read_data(mut file: File) -> CodeRange {
     data[0]..=data[1]
 }
 
-fn calculate_passphrases(pass_range: CodeRange) -> Passphrases {
-    let mut passphrases: Passphrases = vec![];
+fn calculate_passwords(pass_range: CodeRange) -> Passwords {
+    let mut passwords: Passwords = vec![];
     for potential in pass_range {
-        let p_string = format!("{}", potential);
-        let mut chars: Vec<String> = p_string.chars().map(|c| format!("{}", c)).collect();
-        let sorted: Code = {
-            let mut sc = chars.clone();
-            sc.sort();
-            let sc1: String = sc.into_iter().collect();
-            sc1.parse().unwrap()
-        };
-
-        let is_sorted = potential == sorted;
-
-        // if there are at least 2 adjacent duplicates, the deduped version is shorter
-        chars.dedup();
-        let has_adjacent_doubles = chars.len() < 6;
-
-        if is_sorted && has_adjacent_doubles {
-            passphrases.push(Passphrase { code: potential })
+        if is_sorted(potential) && has_adjacent_doubles(potential) {
+            passwords.push(potential)
         }
     }
-    passphrases
+    passwords
 }
 
-fn calculate_passphrases_strict(loose_phrases: Passphrases) -> Passphrases {
+fn calculate_passwords_strict(loose_phrases: Passwords) -> Passwords {
     loose_phrases
         .iter()
         .cloned()
-        .filter(|p| {
-            let p_string = format!("{}", p.code);
-            let chars: Vec<String> = p_string.chars().map(|c| format!("{}", c)).collect();
-            let mut m = std::collections::HashMap::new();
-            for c in chars {
-                *m.entry(c).or_insert(0) += 1
-            }
-            let counts: Vec<u32> = m.values().cloned().collect();
-            counts.contains(&2)
-        })
+        .filter(|p| at_least_one_pair(*p))
         .collect()
+}
+
+// integer-only sorted digit check
+fn is_sorted(input: Code) -> bool {
+    let mut potential = input;
+    let mut digits: Vec<Code> = vec![];
+    while potential > 0 {
+        let part = potential % 10;
+        digits.push(part);
+        potential /= 10;
+    }
+    digits.reverse();
+    let mut sorted = digits.clone();
+    sorted.sort();
+    digits == sorted
+}
+
+fn has_adjacent_doubles(input: Code) -> bool {
+    let mut chars = int2vec(input);
+    chars.dedup();
+    chars.len() < 6
+}
+
+fn int2vec(input: Code) -> Vec<String> {
+    string2vec(format!("{}", input))
+}
+
+fn string2vec(input: String) -> Vec<String> {
+    input.chars().map(|c| c.to_string()).collect()
+}
+
+fn at_least_one_pair(input: Code) -> bool {
+    let chars = int2vec(input);
+    let mut m = std::collections::HashMap::new();
+    for c in chars {
+        *m.entry(c).or_insert(0) += 1
+    }
+    let counts: Vec<u32> = m.values().cloned().collect();
+    counts.contains(&2)
 }
 
 #[cfg(test)]
@@ -99,18 +109,18 @@ mod tests {
 
     #[test]
     fn test_part1_example0() {
-        let pass_phrases = calculate_passphrases(111_110..=223_456);
-        assert_eq!(pass_phrases.contains(&Passphrase { code: 111_111 }), true);
-        assert_eq!(pass_phrases.contains(&Passphrase { code: 223_450 }), false);
-        assert_eq!(pass_phrases.contains(&Passphrase { code: 123_789 }), false);
+        let passwords = calculate_passwords(111_110..=223_456);
+        assert_eq!(passwords.contains(&111_111), true);
+        assert_eq!(passwords.contains(&223_450), false);
+        assert_eq!(passwords.contains(&123_789), false);
     }
 
     #[test]
     fn test_part2_example0() {
-        let pass_phrases = calculate_passphrases(111_110..=223_456);
-        let strict = calculate_passphrases_strict(pass_phrases);
-        assert_eq!(strict.contains(&Passphrase { code: 112_233 }), true);
-        assert_eq!(strict.contains(&Passphrase { code: 123_444 }), false);
-        assert_eq!(strict.contains(&Passphrase { code: 111_122 }), true);
+        let passwords = calculate_passwords(111_110..=223_456);
+        let strict = calculate_passwords_strict(passwords);
+        assert_eq!(strict.contains(&112_233), true);
+        assert_eq!(strict.contains(&123_444), false);
+        assert_eq!(strict.contains(&111_122), true);
     }
 }
